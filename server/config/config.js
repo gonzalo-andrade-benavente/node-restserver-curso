05e22e75a8f2687988824d12fce5a11933ae06c4
Gonzalo Andrade <gonzaloandrade@MacBook-Pro-de-equipamiento.local>
2021-01-31 02:08:40 -0300

    Heroku config

--- server/config/config.js
+++ server/config/config.js
@@ -12,19 +12,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // DB
 
-const userDB = 'root';
-const passDB = 'gonzalito4464';
-const hostDB = 'cluster0.skhs1.mongodb.net';
-
 let urlDB;
-//urlDB = 'mongodb+srv://root:gonzalito4464@cluster0.skhs1.mongodb.net/cafe?retryWrites=true&w=majority';
+
 
 if ( process.env.NODE_ENV === 'dev' ) {
-    urlDB = 'mongodb+srv://root:gonzalito4464@cluster0.skhs1.mongodb.net/cafe?retryWrites=true&w=majority';
-    //urlDB = `mongodb+srv://${ userDB }:${ passDB }@${ hostDB }/cafe?retryWrites=true&w=majority`;
+    urlDB = '';
 } else {
-    urlDB = 'mongodb+srv://root:gonzalito4464@cluster0.skhs1.mongodb.net/cafe_prod?retryWrites=true&w=majority';
-    //urlDB = `mongodb+srv://${ userDB }:${ passDB }@${ hostDB }/cafe_prod?retryWrites=true&w=majority`;
+    urlDB = process.env.NODE_URI;
 }
 
 process.env.URL_DB = urlDB;
