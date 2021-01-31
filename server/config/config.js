47fe12c05633a18e7613a8e7eefe762b612dde1f
Gonzalo Andrade <gonzaloandrade@MacBook-Pro-de-equipamiento.local>
2021-01-31 01:58:41 -0300

    Fix db credentials

--- server/config/config.js
+++ server/config/config.js
@@ -20,9 +20,11 @@ let urlDB;
 //urlDB = 'mongodb+srv://root:gonzalito4464@cluster0.skhs1.mongodb.net/cafe?retryWrites=true&w=majority';
 
 if ( process.env.NODE_ENV === 'dev' ) {
-    urlDB = `mongodb+srv://${ userDB }:${ passDB }@${ hostDB }/cafe?retryWrites=true&w=majority`;
+    urlDB = 'mongodb+srv://root:gonzalito4464@cluster0.skhs1.mongodb.net/cafe?retryWrites=true&w=majority';
+    //urlDB = `mongodb+srv://${ userDB }:${ passDB }@${ hostDB }/cafe?retryWrites=true&w=majority`;
 } else {
-    urlDB = `mongodb+srv://${ userDB }:${ passDB }@${ hostDB }/cafe_prod?retryWrites=true&w=majority`;
+    urlDB = 'mongodb+srv://root:gonzalito4464@cluster0.skhs1.mongodb.net/cafe_prod?retryWrites=true&w=majority';
+    //urlDB = `mongodb+srv://${ userDB }:${ passDB }@${ hostDB }/cafe_prod?retryWrites=true&w=majority`;
 }
 
 process.env.URL_DB = urlDB;
